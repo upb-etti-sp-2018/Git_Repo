7250cdb4c16c4fa647dcb19627e1fb7a0181cc1e
mgiurca <mgiurca.mail@gmail.com>
2018-10-27 19:36:52 +0300

    Tema, ex 1

--- Sp_T1/Tema/T1_1.m
+++ Sp_T1/Tema/T1_1.m
@@ -1,10 +1,21 @@
 T = 2; %perioada in secunde
 F = 1/T; %frecventa semnalului
-t = 0:0.002:1; %vectorul pentru timp cu o rezolutie de 0.002 s = 2 ms
+t = 0:0.002:5; %vectorul pentru timp cu o rezolutie de 0.002 s = 2 ms
 x = square(2*pi*F*t, 25); %functia square genereaza un semnal cu factor de 
                           % umplere de 25%
 x = reduce_amp(x); %functie pentru scadere amplitudinii maxime la jumatate
 
-plot(t,x), grid;
-axis([0 1 -2 2]);
+subplot(3,1,1),plot(t,x,'.-'), grid, title('Rezolutie = 2ms');
 
+
+t = 0:0.02:5;
+x = square(2*pi*F*t, 25);
+x = reduce_amp(x);
+
+subplot(3,1,2),plot(t,x,'.-'),grid, title('Rezolutie = 20ms');
+
+t = 0:0.2:5;
+x = square(2*pi*F*t, 25);
+x = reduce_amp(x);
+
+subplot(3,1,3),plot(t,x, '.-'),grid, title('Rezolutie = 200ms');
